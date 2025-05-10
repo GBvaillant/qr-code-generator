980a3e05c7d6c2a069a20c7d4e208d4324c34368
GBvaillant <vaillantgabriel@gmail.com>
2025-05-10 13:58:00 -0300

    modify class for add localstack and simulate AWS S3

--- src/main/java/com/example/qrcode_generator/infra/S3StorageAdapter.java
+++ src/main/java/com/example/qrcode_generator/infra/S3StorageAdapter.java
@@ -6,7 +6,10 @@ import org.springframework.stereotype.Component;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.net.URI;
 
 @Component
 public class S3StorageAdapter implements StoragePort {
@@ -14,14 +17,21 @@ public class S3StorageAdapter implements StoragePort {
     private final S3Client s3Client;
     private final String bucketName;
     private final String region;
+    private final String endpoint;
 
 
     public S3StorageAdapter(@Value("${aws.s3.bucket.name}")String bucketName,
-                            @Value("${aws.s3.region}")String region) {
+                            @Value("${aws.s3.region}")String region,
+                            @Value("${aws.s3.endpoint}")String endpoint) {
         this.bucketName = bucketName;
         this.region = region;
+        this.endpoint = endpoint;
         this.s3Client = S3Client.builder()
                 .region(Region.of(this.region))
+                .endpointOverride(URI.create(endpoint))
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true)
+                        .build())
                 .build();
     }
     @Override
@@ -34,7 +44,11 @@ public class S3StorageAdapter implements StoragePort {
 
         s3Client.putObject(putObjectRequest, RequestBody.fromBytes(fileData));
 
-        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, fileName);
+        // Retorno para usar localstack como simulador da AWS S3
+        return String.format("%s/%s/%s", endpoint, bucketName, fileName);
+
+//        *Caso use a AWS S3 padrão, descomente a linha abaixo e comente a linha acima*
+//        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, fileName);
     }
 
 }
